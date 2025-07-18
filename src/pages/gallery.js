837d8b8c28e10e6c088a4c87d5c6df2da618adaa
Derrick Rioba Ongwae <riobad74@gmail.com>
2025-07-18 17:27:49 +0300

    removed some art

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -10,15 +10,15 @@ const artworks = [
   { title: "skull", imageUrl: "/images/Skull.jpg", description: "A retro-themed digital painting." },
   { title: "mosaic", imageUrl: "/images/Mosaic-skull.jpg", description: "Graffiti meets nature." },
   { title: "Colored-skull", imageUrl: "/images/Colored-skull.jpg", description: "Graffiti meets nature." },
-  { title: "circle", imageUrl: "/images/circle.jpg", description: "Graffiti meets nature." },
-  { title: "good", imageUrl: "/images/good.jpg", description: "Graffiti meets nature." },
-  { title: "peace", imageUrl: "/images/peace.jpg", description: "Graffiti meets nature." },
-  { title: "Robot", imageUrl: "/images/robots.jpg", description: "Graffiti meets nature." },
-  { title: "3-0", imageUrl: "/images/3-0.jpg", description: "Graffiti meets nature." },
-  { title: "triumph", imageUrl: "/images/triumph.jpg", description: "Graffiti meets nature." },
-  { title: "ls-400", imageUrl: "/images/ls-400.jpg", description: "Graffiti meets nature." },
+  // { title: "circle", imageUrl: "/images/circle.jpg", description: "Graffiti meets nature." },
+  // { title: "good", imageUrl: "/images/good.jpg", description: "Graffiti meets nature." },
+  // { title: "peace", imageUrl: "/images/peace.jpg", description: "Graffiti meets nature." },
+  // { title: "Robot", imageUrl: "/images/robots.jpg", description: "Graffiti meets nature." },
+  // { title: "3-0", imageUrl: "/images/3-0.jpg", description: "Graffiti meets nature." },
+  // { title: "triumph", imageUrl: "/images/triumph.jpg", description: "Graffiti meets nature." },
+  // { title: "ls-400", imageUrl: "/images/ls-400.jpg", description: "Graffiti meets nature." },
   { title: "still-life", imageUrl: "/images/still life.jpg", description: "Graffiti meets nature." },
-  { title: "house", imageUrl: "/images/house.jpg", description: "Graffiti meets nature." },
+  // { title: "house", imageUrl: "/images/house.jpg", description: "Graffiti meets nature." },
 ];
 
 export default function Gallery() {
