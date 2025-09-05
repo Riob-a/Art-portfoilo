e38414bf76cbd508def5283f2e85a22d9e7ed435
Derrick Rioba Ongwae <riobad74@gmail.com>
2025-09-05 11:28:57 +0300

    added new

--- src/data/artworks.js
+++ src/data/artworks.js
@@ -2,14 +2,17 @@ const createSlug = (title) =>
   title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
 
 const artworks = [
-  { title: "skull", imageUrl: "/images/Skull.jpg", description: "Potrait of Human skull made with both pencil and paint. This was my third attempt with the aim of mixing drawing an painting",category:"Pencil/Paint", year:"2022"   },
-  { title: "mosaic", imageUrl: "/images/Mosaic-skull.jpg", description: "Potrait of a Human skull Drawn entirely out of paint.This was My Second yearly study of the human skull",category:"Paint", year:"2020"   },
-  { title: "Colored-skull", imageUrl: "/images/Colored-skull.jpg", description: "Potrait of the Human Skull Drawn with both pencil and paint. This is my fourth attempt going for more detail than the third",category:"Pencil/Paint", year:"2025"   },
-  { title: "still-life", imageUrl: "/images/still life.jpg", description: "Simple still life of a fiew bottles and an apple",category:"Pencil" , year:"2021" },
-  { title: "Hanging-branch", imageUrl: "/images/Branch.jpg", description: "A composition consisting of a hanging branch, drawn entirely digitally.", category:"Digital" , year:"2025" },
-  { title: "Spots", imageUrl: "/images/Spots.jpg", description: "Spots and patterns.", category:"Digital", year:"2025" },
-  { title: "Rose", imageUrl: "/images/Rose.jpg", description: "A beautiful rose drawn with intricate details.", category:"Digital", year:"2025"  },
-  { title: "Cyclops", imageUrl: "/images/Cyclops.jpg", description: "Cyclops .", category:"Digital", year:"2025"  }
+  { title: "skull", imageUrl: "/images/Skull.jpg", description: "Potrait of Human skull made with both pencil and paint. This was my third attempt with the aim of mixing drawing an painting", category: "Pencil/Paint", year: "2022" },
+  { title: "mosaic", imageUrl: "/images/Mosaic-skull.jpg", description: "Potrait of a Human skull Drawn entirely out of paint.This was My Second yearly study of the human skull", category: "Paint", year: "2020" },
+  { title: "Colored-skull", imageUrl: "/images/Colored-skull.jpg", description: "Potrait of the Human Skull Drawn with both pencil and paint. This is my fourth attempt going for more detail than the third", category: "Pencil/Paint", year: "2025" },
+  { title: "Hanging-branch", imageUrl: "/images/Branch.jpg", description: "A composition consisting of a hanging branch, drawn entirely digitally.", category: "Digital", year: "2025" },
+  { title: "Touch", imageUrl: "/images/Touch.jpg", description: "Stylized version of Hands of god .", category: "Digital", year: "2025" },
+
+  { title: "Spots", imageUrl: "/images/Spots.jpg", description: "Spots and patterns.", category: "Digital", year: "2025" },
+    { title: "still-life", imageUrl: "/images/still life.jpg", description: "Simple still life of a fiew bottles and an apple", category: "Pencil", year: "2021" },
+  { title: "Rose", imageUrl: "/images/Rose.jpg", description: "A beautiful rose drawn with intricate details.", category: "Digital", year: "2025" },
+  { title: "Cyclops", imageUrl: "/images/Cyclops.jpg", description: "Cyclops .", category: "Digital", year: "2025" },
+
 
 ].map(art => ({
   ...art,
